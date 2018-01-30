148d0a6d5816edd56b6d44b4b95a02106b3fd6ab
17viu18m <v2017.17viu18m@outlook.com>
2018-01-30 22:40:03 +0300

    check

--- sources/demo.cpp
+++ sources/demo.cpp
@@ -1,11 +1,13 @@
-#include <print.hpp>
+"Copyright 2018 NO COPYRIGHTS>"
 #include <cstdlib>
+#include <print.hpp>
+$log_path = log_path
 
 int main(int argc, char** argv) {
         std::string text;
-std::string log_path = std::getenv("LOG_PATH");
-	std::ofstream out(log_path, std::ios_base::app);
-        while(std::cin >> text) {
+std::string $log_path = std::getenv("LOG_PATH");
+        std::ofstream out(log_path, std::ios_base::app);
+        while (std::cin >> text) {
                 print(text, out);
                 out << std::endl;
         }
